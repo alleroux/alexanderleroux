628174ee8c20612f0df42626245fde9adc7c58a1
Alexander Leroux <atroxodisse@gmail.com>
2016-10-02 20:46:19 -0700

    Added new favorite icons.  Changed some text.  Updated some styles.  Add realestatecenter to portfolio

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,19 +18,38 @@ angular.module('siteApp')
      $scope.portfolios = [
       {
         category: "My Day Job",
+        name: "Real Estate Digital",
+        img: "portfolio/realestatecenter.png",
+        notes: 'I build real estate websites for <a href="http://www.realestatedigital.com" target="_blank">RED</a>',
+        link: "https://realestatecenter.bankofamerica.com",
+        alt: "Bank of America Real Estate Center screen shot"
+      },
+      {
+        category: "My Other Job",
         name: "MelRok Energy IoT",
-        img: "portfolio/portfolio-melrok.png"
+        img: "portfolio/portfolio-melrok.png",
+        notes: "I built melrok.com and I'm building the user interface for their Energy Management Platform",
+        link: "http://melrok.com",
+        alt: "Melkrok.com screen shot"
       },
       {
         category: "Enterprise Security Software",
         name: "McAfee Vulnerability Manager",
-        img: "portfolio/mvm.png"
+        img: "portfolio/mvm.png",
+        notes: "I served as the front end engineer for the McAfee Vulnerability Manager product for 8 years",
+        link: "http://intelsecurity.com",
+        alt: "McAfee Vulnerability Manager 7.5 screen shot"
       },
+      {},
       {
         category: "PHP Security Consulting",
         name: "Wisilica PHP Server Application Security Review",
-        img: "portfolio/secure-php.png"
-      }
+        img: "portfolio/secure-php.png",
+        notes: "I performed a security review of their API services",
+        link: "http://wisilica.com/",
+        alt: "PHP Lock logo"
+      },
+      {}
      ];
 
     (function($) {
--- app/scripts/directives/portfolio.js
+++ app/scripts/directives/portfolio.js
@@ -9,7 +9,16 @@
 angular.module('siteApp')
   .directive('portfolio', function () {
     return {
-      template: '<a href="#" class="portfolio-box"><img src="images/{{portfolio.img}}" class="img-responsive" alt=""><div class="portfolio-box-caption"><div class="portfolio-box-caption-content"><div class="project-category text-faded">{{portfolio.category}}</div><div class="project-name">{{portfolio.name}}</div></div></div></a>',
+      template: "\
+      <a href=\"{{portfolio.link}}\" target=\"_blank\" class=\"portfolio-box\">\
+      	<img src=\"images/{{portfolio.img}}\" class=\"img-responsive\" alt=\"{{portfolio.alt}}\">\
+      	<div class=\"portfolio-box-caption\"><div class=\"portfolio-box-caption-content\">\
+      		<div class=\"project-category text-faded\">{{portfolio.category}}</div>\
+      		<div class=\"project-name\">{{portfolio.name}}</div>\
+      		<div ng-bind-html=\"portfolio.notes\" class=\"portfolio-notes\"></div>\
+      	</div>\
+      	</div>\
+      </a>",
       restrict: 'A',
       scope: {
         portfolio: '='
